3c03801c6bc60b21516e11b1f6a6c40b45881ba0
WebGameCreator <138807724+WebGameCreator@users.noreply.github.com>
2024-03-18 20:57:28 +0100

    Installed monaco editor

--- main.js
+++ main.js
@@ -11,8 +11,14 @@ if (query.has("a")) {
 	document.querySelector("html").innerHTML = decode(query.get("a"));
 }
 else {
+	const editor = monaco.editor.create(document.getElementById('editor'), {
+		value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+		language: 'javascript'
+	});
 	document.getElementById("content").style.display = "block";
+	/*
 	document.getElementById("openButton").onclick = function () {
 		window.open("https://webgamecreator.github.io?a=" + encode(document.getElementById("input").value), "_self");
 	}
+	*/
 }
