ac91800f7c718d0fdfc2fc8879e68a8a2551ba47
WebGameCreator <138807724+WebGameCreator@users.noreply.github.com>
2024-03-18 18:59:34 +0100

    Added base64 converter
    - changed to url safe base64
    - added base64 converter

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+function encode(input) {
+	return btoa(input).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, ".");
+}
+
+function decode(input) {
+	return atob(input.replace(/-/g, "+").replace(/_/g, "/").replace(/\./g, "="));
+}
+
 const query = new URLSearchParams(location.search);
 if (query.has("a")) {
 	document.open();
-	document.write(atob(query.get("a")));
+	document.write(decode(query.get("a")));
 	document.close();
 }
 else {
 	document.getElementById("content").style.display = "block";
+}
+
+function openUrl() {
+	window.open("https://webgamecreator.github.io/" + encode(document.getElementById("input")), "_self");
 }
